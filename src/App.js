c834f6d7da968d9a3949e3e746087f432620ff8e
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-04-17 12:32:17 -0500

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import { useMidiInputs } from 'react-midi-context';
+import { Canvas, useFrame } from 'react-three-fiber';
 import './App.css';
 
-function App() {
+function Box(props) {
+  const mesh = useRef();
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'skyblue'} />>
+    </mesh>
   );
+}
+
+function App() {
+  const [midiInput] = useMidiInputs({ debug: true });
+  const [boxes, setBoxes] = useState([]);
+
+  useEffect(() => {
+    midiInput.addListener('noteon', 'all', (e) => {
+      // setBoxes(prevBoxes => [...prevBoxes, <Box position])
+    });
+  })
+
+  return (
+    <>
+      yo 🛹
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </>
+  )
 }
 
 export default App;
---
+++ src/my-component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MyComponent(props) {
+  console.log(props);
+
+  return <h1>Yo!</h1>;
+}
+
+export default MyComponent;
