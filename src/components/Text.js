fa7e8600ea8f7e679849706b17c9228bd159e863
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-04-30 14:14:26 -0500

    Add proof-of-concept Text component w/ custom font

---
+++ src/components/Text.js
@@ -0,0 +1,25 @@
+import React, { useState, useMemo } from 'react';
+import * as Three from 'three';
+import fantasque from '../fonts/fantasque';
+
+const { FontLoader } = Three;
+
+export default function Text({ children, vAlign = 'center', hAlign = 'center', size = 1, color = '#000000', position, ...props }) {
+  const [font] = useState(() => new FontLoader().parse(fantasque));
+  const textConfig = useMemo(
+    () => ({
+      font,
+      size: 1,
+      height: 1,
+      curveSegments: 32
+    }),
+    [font]
+  );
+
+  return (
+    <mesh>
+      <textGeometry attach="geometry" position={position} args={[children, textConfig]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  );
+}
