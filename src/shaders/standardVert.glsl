fdc2d3f755e33c42fb1e71e243ff26b40cd53f2f
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-04-30 14:11:31 -0500

    Add shaders and fonts

--- src/shaders/standardVert.glsl
+++ src/shaders/standardVert.glsl
@@ -1,5 +1,11 @@
+precision highp float;
+
 varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    }
+uniform float bend;
+
+void main() {
+  vUv = uv;
+  vec3 pos = position;
+  pos.z = sin(position.y * (bend * 4.0));
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+}
--- src/shaders/waveFrag.glsl
+++ src/shaders/waveFrag.glsl
@@ -2,12 +2,14 @@ varying vec2 vUv;
 
 uniform vec3 color1;
 uniform vec3 color2;
-uniform float bend;
+uniform float clock;
 
 void main() {
   float degToRad = 0.01745329;
-  float colorMix = vUv.y - vUv.x + bend;
+  float colorMix = vUv.y - vUv.x + cos(clock / 1000.0);
+
+  vec3 color = mix(color1, color2, colorMix);
 
   gl_FragColor.a = 1.0;
-  gl_FragColor.rgb = mix(color1, color2, colorMix);
+  gl_FragColor.rgb = color;
 }
